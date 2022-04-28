1a17714e52e3af83b80b90b8f1b74453ee920089
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-28 15:05:16 -0400

    Test logic (calculate.js and operate.js)

---
+++ src/components/__test__/Navbar.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import '@testing-library/jest-dom'
+import Navbar from '../Navbar';
+
+describe('Navigation page', () => {
+  it('rNavigation container', () => {
+    render(
+      <Router>
+        <Navbar />
+      </Router>
+    );
+    const navBar = screen.getByRole('navigation');
+    expect(navBar).toBeInTheDocument();
+  });
+  
+  it('Number of links', () => {
+    render(
+      <Router>
+        <Navbar />
+      </Router>
+    );
+    const linkElement = screen.getAllByRole('link');
+    expect(linkElement.length).toBe(3);
+  });
+});
